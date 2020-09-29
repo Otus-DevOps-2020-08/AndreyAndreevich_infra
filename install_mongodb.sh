af171704a2504708bdf8a65dc046a7c3aec0b2eb
AndreyAndreevich <l.a.r.p@yandex.ru>
2020-09-29 14:22:55 +0400

    Updated script

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" \
     | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
