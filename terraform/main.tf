74926be33cfc6fbb07dcb4f3a8cd29e1d1e81492
AndreyAndreevich <l.a.r.p@yandex.ru>
2020-09-30 12:44:35 +0400

    Done hw

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,57 @@
-provider "yandex" {}
-
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
       version = "~> 0.35"
     }
   }
 }
 
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
+  zone = var.app_zone
 
   resources {
-    cores  = 1
+    cores  = 2
     memory = 2
   }
 
   boot_disk {
     initialize_params {
-      image_id = "fd8g17150trtl64i5i0k"
+      image_id = var.image_id
     }
   }
 
   network_interface {
-    subnet_id = "b0cnunlut438e9ntfb0u"
+    subnet_id = var.subnet_id
     nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    host        = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user        = "ubuntu"
+    agent       = false
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
   }
 }
