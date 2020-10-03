96ee90c989f9ae00a1329895203b17bd3d384ec7
AndreyAndreevich <l.a.r.p@yandex.ru>
2020-10-03 15:59:48 +0400

    Added prod and stage env

---
+++ terraform/stage/variables.tf
@@ -0,0 +1,33 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  default     = "ru-central1-a"
+}
+variable app_zone {
+  description = "App Zone"
+  default     = "ru-central1-a"
+}
+variable service_account_key_file {
+  description = "key.json"
+}
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+variable reddit_app_port {
+  description = "Reddit app port"
+  default     = 9292
+}
+variable app_disk_image {
+  description = "Disk image for reddit app"
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+}
+variable subnet_id {
+  description = "Subnets for modules"
+}
---
+++ terraform/vpc/outputs.tf
@@ -0,0 +1,3 @@
+output "external_subnet_id" {
+  value = module.vpc.external_subnet_id
+}
